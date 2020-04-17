040b657d0aa25daa1285c1fa98b6fa5fa842e406
Loc Luong <loc.luong09@gmail.com>
2020-04-17 14:32:17 -0600

    Test with Himmelblau function

--- example.py
+++ example.py
@@ -6,26 +6,35 @@ from particle_swarm_optimization import PSO
 
 # Define the objective function
 def func(x,y):
-	#Function with 2 varibel x,y
+	# McCormick function
 	return np.sin(x+y) + (x-y)**2 -1.5*x + 2.5*y + 1
-
 #set contraint for varibles
 lo_bounds = [-1.5,-3]
 up_bounds = [4,4]
 
+def func1(x,y):
+	# Himmelblau function
+	return (x**2 + y -11)**2 + (x+y**2 - 7)**2
+
+lo_bounds1 = [-5,-5]
+up_bounds1 = [5,5]
+
+def func2(x,y):
+	return (x**2 + y**2)
+
 def main():
 	# Define objective class
-	objective_class = Objective_function(func,2,lo_bounds, up_bounds)
+	objective_class = Objective_function(func1, 2,lo_bounds1, up_bounds1)
 
 	#set PSO optimizer
 	pso_opt = PSO(nb_generations = 10,
-				nb_populations = 50,
+				nb_populations = 20,
 				objective_class = objective_class,
-				w = 0.5,
-				c1 = 0.8,
-				c2 = 0.9,
-				max_velocity = 10,
-				min_velocity = -10)
+				w = 0.72984,
+				c1 = 1.496172,
+				c2 = 1.496172,
+				max_velocity = 20,
+				min_velocity = -20)
 
 	pso_opt.evolve()
 
